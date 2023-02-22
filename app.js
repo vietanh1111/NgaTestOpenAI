4c3a15bdf63e9c2fde47c746a1b8d3100a4c0af8
anh.nguyenviet6@gameloft.com <vietanh6>
2023-02-22 20:37:40 +0700

    - change incoming webhook to test

--- app.js
+++ app.js
@@ -67,7 +67,9 @@ async function requestGetOpenAIMsgForChatBot(question, mmUrl) {
 }
 
 function sendMessageToMM(msg, request_url) {
-    let url = "https://chat.gameloft.org/hooks/yzi9zs5jw7naikmgiqtmf3u8bh"
+    
+    // let url = "https://chat.gameloft.org/hooks/yzi9zs5jw7naikmgiqtmf3u8bh"
+    let url = "https://chat.gameloft.org/hooks/63gsjdxiy7drug4bpouo6rd7ir"
     request.post(
         url,
         { json: { "text": msg } },
