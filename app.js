1ede4b2eb189050f4d48026ad18eddd70a1c9410
anh.nguyenviet6@gameloft.com <vietanh6>
2023-02-24 11:18:38 +0700

    - fix async and set LIMIT to 2500 token

--- app.js
+++ app.js
@@ -30,68 +30,67 @@ app.use(function (req, res, next) {
     next();
 });
 
-
+OPENAI_COMPLETIONS_MAX_TOKEN = 4000
+OPENAI_COMPLETIONS_ALLOW_WORDS = 2500 // ~75% MAX TOKEN
 let conversation = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly."
-async function requestGetOpenAIMsgForChatBot(raw_question, mmUrl) {
+async function requestGetOpenAIMsgForChatBot(input_question) {
     console.log("requestGetOpenAIMsgForChatBot ")
 
-    question = "\nHuman:" + raw_question + "\nAI:"
+    let question = "\nHuman:" + input_question + "\nAI:"
     conversation = conversation + question
 
-    console.log("conversation=" + conversation)
-
-    let request_data = {
-        model: "text-davinci-003",
-        prompt: conversation,
-        temperature: 0.1,
-        max_tokens: 2000,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.6,
-        stop: [" Human:", " AI:"],
-    }
-
-    try {
-
-        const completion = await openaiObj.createCompletion(request_data);
-        res = completion.data.choices[0].text
-        res = res.trim()
-        conversation = conversation + res
-
-        console.log("conversation=" + conversation)
-        sendMessageToMM(res, raw_question, mmUrl)
-    } catch (error) {
-        console.log("get error")
-        console.log(error)
-    }
-}
-
-function sendMessageToMM(msg, question, request_url) {
-    console.log("sendMessageToMM")
-    let url = "https://chat.gameloft.org/hooks/nw81wo1bc3rjzq5jrmpyeztd3o"
-    // let url = "https://chat.gameloft.org/hooks/yzi9zs5jw7naikmgiqtmf3u8bh"
-    // let url = "https://chat.gameloft.org/hooks/63gsjdxiy7drug4bpouo6rd7ir"
-    let messageMM = "**Tớ: **" + question + "\n**Bạn AI: **" + msg
-    console.log("messageMM=" + messageMM)
-    request.post(
-        url,
-        { json: { "text": messageMM } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
-            } else {
-                console.log("got error")
-            }
+    console.log("begin conversation=" + conversation)
+    console.log("words in conversation=" + conversation.split(" ").length)
+    if (conversation.split(" ").length < OPENAI_COMPLETIONS_ALLOW_WORDS) {
+        let request_data = {
+            model: "text-davinci-003",
+            prompt: conversation,
+            temperature: 0.1,
+            max_tokens: OPENAI_COMPLETIONS_MAX_TOKEN,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.6,
+            stop: [" Human:", " AI:"],
         }
-    )
+
+        try {
+            let completion = await openaiObj.createCompletion(request_data);
+            let res = completion.data.choices[0].text
+            res = res.trim()
+            conversation = conversation + res
+
+            console.log("end conversation=" + conversation)
+
+            let messageMM = "**Tớ: **" + input_question + "\n**Bạn AI: **" + res
+            res = await sendMessageToMM(messageMM)
+            console.log("requestGetOpenAIMsgForChatBot get done")
+            return res
+
+        } catch (error) {
+            console.log("requestGetOpenAIMsgForChatBot get error")
+            console.log(error)
+            return error
+        }
+    } else {
+        conversation = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly."
+        let messageMM = "**Tớ: **" + input_question + "\n**Bạn AI: **" + "Rất tiếc, tôi không thể nhớ được tất cả những gì bạn nói, tôi đang xóa ký ức và chúng ta sẽ bắt đầu lại nha :hugging_face: :hugging_face: :hugging_face: "
+        await sendMessageToMM(messageMM)
+        return "ok and clear conversation"
+    }
+
 }
 
-
-function chatBot(question) {
-    console.log("chatBot")
-
-    console.log("question=" + question)
-    requestGetOpenAIMsgForChatBot(question)
+async function sendMessageToMM(msg) {
+    console.log("sendMessageToMM")
+    let req_method = "POST"
+    let req_url = "https://chat.gameloft.org/hooks/nw81wo1bc3rjzq5jrmpyeztd3o"
+    // let req_url = "https://chat.gameloft.org/hooks/63gsjdxiy7drug4bpouo6rd7ir"
+    let req_data = JSON.stringify({
+        text: msg,
+        user_name: "anh.nguyenviet6"
+    })
+    let result = await getRequestResponse(req_method, req_url, req_data)
+    return result
 }
 
 app.post('/doChatOpenAI_slash', function (req, res) {
@@ -102,7 +101,7 @@ app.post('/doChatOpenAI_slash', function (req, res) {
             console.log(data)
 
             let params = queryString.parse(data);
-            let question = params.text;            
+            let question = params.text;
             chatBot(question)
             res.end("doChatOpenAI done")
         })
@@ -118,21 +117,22 @@ app.post('/doChatOpenAI_ow', function (req, res) {
             jsonData = JSON.parse(data)
             if (jsonData.text.startsWith("OpenAI Chat:")) {
                 let question = jsonData.text.replace('OpenAI Chat:', '');
-                chatBot(question)
-            }          
-            
-            res.end("doChatOpenAI done")
+                let response = await requestGetOpenAIMsgForChatBot(question)
+                console.log("DONE")
+                res.end(response)
+            } else {
+                res.end("doChatOpenAI nothing")
+            }
         })
     }
 })
-
 
 app.post('/resetConversation', function (req, res) {
     if (req.method == 'POST') {
         req.on('data', async function (data) {
             data = data.toString()
             console.log("resetConversation for the data")
-            conversation = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly."    
+            conversation = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly."
 
 
             res.end("resetConversation done")
@@ -140,6 +140,43 @@ app.post('/resetConversation', function (req, res) {
     }
 })
 
+
+async function makeRequest(req_method, req_url, req_data) {
+    console.log("makeRequest START")
+    const options = {
+        url: req_url,
+        method: req_method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: req_data
+    };
+
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        });
+    });
+}
+
+async function getRequestResponse(req_method, req_url, req_data) {
+    console.log("getRequestResponse START")
+    try {
+        const response = await makeRequest(req_method, req_url, req_data);
+        console.log("parseResponse");
+        console.log(response);
+        // Do something with the response
+        return response
+    } catch (error) {
+        console.log("parseResponse error");
+        console.error(error);
+        return "getRequestResponse error"
+    }
+}
 
 
 var server = app.listen(port, function () {
